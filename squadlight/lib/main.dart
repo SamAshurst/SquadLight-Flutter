99c767dc3a815fc16eb2e9b9f6d5f347b30dd8f7
Kieron Day <kieron.day@email.com>
2022-03-21 16:51:24 +0000

    Added button to fetch user's current location and display at the top of the app

--- squadlight/lib/main.dart
+++ squadlight/lib/main.dart
@@ -4,9 +4,11 @@ import 'package:latlong2/latlong.dart';
 import 'package:location/location.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: MapPage(),
-  ));
+  runApp(
+    const MaterialApp(
+      home: MapPage(),
+    ),
+  );
 }
 
 class MapPage extends StatefulWidget {
@@ -17,13 +19,59 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  
+// Start of copied code 
+
+late bool _serviceEnabled;
+  late PermissionStatus _permissionGranted;
+  LocationData? _userLocation;
+
+  Future<void> _getUserLocation() async {
+    Location location = Location();
+
+    // Check if location service is enable
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    // Check if permission is granted
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    final _locationData = await location.getLocation();
+    setState(() {
+      _userLocation = _locationData;
+    });
+  }
+  //End of copied code
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getUserLocation,
+        child: const Text('Get User Location') 
+      ),
       body: Center(
         child: Container(
-          child: (Column(
+          child: Column(
             children: [
+              Text('${_userLocation}'),
               Flexible(
                 child: FlutterMap(
                   options:
@@ -38,14 +86,15 @@ class _MapPageState extends State<MapPage> {
                         Marker(
                           point: LatLng(53.472164, -2.238193),
                           builder: (ctx) => Icon(Icons.pin_drop),
+                          
                         ),
                       ],
                     )
                   ],
                 ),
-              )
+              ),
             ],
-          )),
+          ),
         ),
       ),
     );
