c473966ba019a50051e250eb7ca4f6c9962eba0a
Root-2 <routledge.dan@gmail.com>
2022-03-22 11:18:37 +0000

    User location now pinging properly.

--- squadlight/lib/main.dart
+++ squadlight/lib/main.dart
@@ -19,10 +19,9 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  
-// Start of copied code 
-
-late bool _serviceEnabled;
+// Start of copied code
+  LatLng userLoc = LatLng(53.472164, -2.238193);
+  late bool _serviceEnabled;
   late PermissionStatus _permissionGranted;
   LocationData? _userLocation;
 
@@ -50,51 +49,41 @@ late bool _serviceEnabled;
     final _locationData = await location.getLocation();
     setState(() {
       _userLocation = _locationData;
+      userLoc = LatLng(_locationData.latitude!, _locationData.longitude!);
     });
   }
   //End of copied code
-
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: _getUserLocation,
-        child: const Text('Get User Location') 
-      ),
+          onPressed: _getUserLocation, child: const Text('Get User Location')),
       body: Center(
-        child: Container(
-          child: Column(
-            children: [
-              Text('${_userLocation}'),
-              Flexible(
-                child: FlutterMap(
-                  options:
-                      MapOptions(center: LatLng(53.472164, -2.238193), zoom: 8),
-                  layers: [
-                    TileLayerOptions(
-                        urlTemplate:
-                            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                        subdomains: ['a', 'b', 'c']),
-                    MarkerLayerOptions(
-                      markers: [
-                        Marker(
-                          point: LatLng(53.472164, -2.238193),
-                          builder: (ctx) => Icon(Icons.pin_drop),
-                          
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+        child: Column(
+          children: [
+            Text('$_userLocation'),
+            Flexible(
+              child: FlutterMap(
+                options:
+                    MapOptions(center: LatLng(53.472164, -2.238193), zoom: 8),
+                layers: [
+                  TileLayerOptions(
+                      urlTemplate:
+                          "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c']),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                        point: LatLng(userLoc.latitude, userLoc.longitude),
+                        builder: (ctx) => const Icon(Icons.pin_drop),
+                      ),
+                    ],
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
